a3b344e42b88474590a844083f0635cac7bec10e
lcg <BigDatalcg1@163.com>
2018-12-11 19:04:39 +0800

    add 3

--- project_test/src/main/java/com/bjavc/Test.java
+++ project_test/src/main/java/com/bjavc/Test.java
@@ -6,5 +6,7 @@ package com.bjavc;
 public class Test {
     public static void main(String[] args) {
         System.out.println("1111111111111111");
+        System.out.println("2222222222222222");
+        System.out.println("hape");
     }
 }

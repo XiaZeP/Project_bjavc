098f37077ecc2d82c104b3e6c9d08e58906c9675
sqs <289330634@qq.com>
2018-12-11 18:42:20 +0800

    updata 88

--- project_test/src/main/java/com/bjavc/Test.java
+++ project_test/src/main/java/com/bjavc/Test.java
@@ -6,5 +6,6 @@ package com.bjavc;
 public class Test {
     public static void main(String[] args) {
         System.out.println("1111111111111111");
+        System.out.println("8888888888888");
     }
 }

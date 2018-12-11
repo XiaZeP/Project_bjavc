457581272add4640e280d6fb517027586762dd3b
xzp <15701324243@163.com>
2018-12-11 16:40:53 +0800

    初始提交

---
+++ project_test/src/main/java/com/bjavc/Test.java
@@ -0,0 +1,10 @@
+package com.bjavc;
+
+/**
+ * Created by xiaze on 2018/12/11.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111");
+    }
+}
